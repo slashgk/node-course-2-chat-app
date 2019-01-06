2cd396191fe4802eb6bfe546f5af0d840b119659
GAURAV KUMAR <gaurav.grd@gmail.com>
2019-01-06 15:54:56 +0530

    Emit newMesaage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function()  {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        to: 'slash',
-        text: 'Hey this is slash'
-    });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,20 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage',newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage',message);
+        io.emit('newMessage', {
+            from: message.from,
+            text:   message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
-
-    socket.emit('newMessage', {
-        from: 'slash',
-        test: 'Hey! Whats up',
-        createdAt: 123
-    });
-
 });
 
 server.listen(port, () => {
